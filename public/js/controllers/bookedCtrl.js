1c3e7fb65a85c266220541eaa030a3399c13f6be
chegior <chegiorca@hotmail.com>
2017-07-29 10:31:13 -0600

    Adjusting some features

--- public/js/controllers/bookedCtrl.js
+++ public/js/controllers/bookedCtrl.js
@@ -3,7 +3,9 @@ angular.module('apapnow').controller('bookedCtrl',function($scope,loginSrv){
 
 
   //this function should recieve USERNAME and PASSWORD and search if DB already has that
-  console.log('booked', loginSrv.user)
+  console.log('booked', loginSrv.user);
+  $scope.customer = loginSrv.user;
+  
 
 
 })
