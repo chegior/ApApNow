658ee515e64f0d8549458e028e6ba6b2f15456ba
chegior <chegiorca@hotmail.com>
2017-07-31 23:34:41 -0600

    stock on GOOGLE CALENDAR API

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'googleAuth' : {
+    'clientID':'798988379818-9qe0vgog0p5c7pj67dngkh0489c9sml8.apps.googleusercontent.com',
+    'clientSecret':'qvQKTzJEs2VnC2-ein1vx7XP',
+    'callbackURL':'http://localhost:3000/auth/google/callback'
+
+ }
+}
--- index.js
+++ index.js
@@ -2,6 +2,61 @@ var express = require ('express');
 var bodyParser = require ('body-parser');
 var cors = require ('cors');
 var massive = require ('massive');
+var cookieParser = require('cookie-Parser')
+
+var session = require('express-session');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var gcal = require('google-calendar');
+var port = 3000;
+var app = module.exports = express();
+
+var configAuth = require('./config/auth.js')
+
+app.use(cookieParser());
+app.use(session({secret:'un secreto'}));
+app.use(bodyParser.json());
+app.use(passport.initialize());//initialize functions on the passport
+//app.use(passport.session());//we want to store data on our session
+//////////AUTHENTICATION FOR GOOGLE/////////////////////////////////
+//////////AUTHENTICATION FOR GOOGLE/////////////////////////////////
+
+passport.use(new GoogleStrategy({
+    clientID: configAuth.googleAuth.clientID,
+    clientSecret: configAuth.googleAuth.clientSecret,
+    callbackURL: configAuth.googleAuth.callbackURL,
+    //scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar']
+  },
+    function(accessToken, refreshToken, profile, done) {
+     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+       return done(err, user);
+     });
+   }
+));
+
+//
+// if(err){
+//   return done(err);
+// }
+// if(user){
+//   return done(null,user);
+// }
+// else{
+//   console.log("HERE:" + user);
+//     return (user);
+// }
+
+
+app.get('/auth/google', passport.authenticate('google', { scope:'https://www.googleapis.com/auth/calendar'}));
+
+app.get('/auth/google/callback',
+passport.authenticate('google', { succesRedirect: '/auth/google', failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+//////////EOF AUTHENTICATION FOR GOOGLE/////////////////////////////////
+//////////EOF AUTHENTICATION FOR GOOGLE/////////////////////////////////
+
 
 var connectionString = 'postgres://postgres:1603chedro@localhost/AAN-db';
 var dbCtrl = require ('./controllers/dbCtrl');
@@ -9,12 +64,11 @@ var dbCtrl = require ('./controllers/dbCtrl');
 massive( connectionString ).then( dbInstance => {
   app.set('db', dbInstance);});
 
-var port = 3000;
-var app = module.exports = express();
+
 
 app.use(express.static('public'));
 
-app.use(bodyParser.json());
+
 
 app.get('/api/customers',dbCtrl.getCustomers);
 
